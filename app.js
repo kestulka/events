c653174e2ff2b5be0ab5b4434fbcbb1e01a0cde1
kestulka <kgirdiunas0@gmail.com>
2024-01-29 18:42:44 +0200

    retry

---
+++ app.js
@@ -0,0 +1,7 @@
+import { firebaseConfig } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
